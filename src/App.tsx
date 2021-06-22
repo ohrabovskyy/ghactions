38a0a49221f0029659b136d671fbc5a096c2919d
Oleksandr Hrabovskyy <oleksandr.hrabovskyy@boystown.org>
2021-06-21 23:00:23 -0500

    Added a simple change for testing purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with GiHub Actions
         </a>
       </header>
     </div>
